f350a289fdb10765595d575078853ece51dd97eb
shagia <usenet@shagia.me>
2020-02-26 20:15:00 -0800

    make menu bar

--- src/components/headerMenu.js
+++ src/components/headerMenu.js
@@ -8,27 +8,25 @@ export default () => {
   return (
     <div>
       <header className="site-head">
-        <div className="nav-flex-container">
-          <nav className="nav-hor head-bar">
-            <ul>
-              <li>
-                <div className="logo-container">
-                  <Link to={`/`}><img className="logo-img-src" src={logo}></img></Link>
-                </div>
-              </li>
-              <li>
-                <Link to={`/`}>HOME</Link>
-              </li>
-              <li>
-                <Link to={`/artists/`}>ARTISTS</Link>
-              </li>
-              <li>
-                <Link to={`/contact/`}>CONTACT</Link>
-              </li>
-            </ul>
-          </nav>
+        <nav className="nav-flex-container nav-hor head-bar">
+          <ul className="nav-items">
+            <li className="logo-container">
+              <Link to={`/`}>
+                <img className="logo-img-src" src={logo}></img>
+              </Link>
+            </li>
+            <li>
+              <Link to={`/`}>HOME</Link>
+            </li>
+            <li>
+              <Link to={`/artists/`}>ARTISTS</Link>
+            </li>
+            <li>
+              <Link to={`/contact/`}>CONTACT</Link>
+            </li>
+          </ul>
           <div className="load">LOADING</div>
-        </div>
+        </nav>
       </header>
     </div>
   )
